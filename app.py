d0d4e8016fa9db646148aa003c67e938af0f0b1a
ebety45 <251075@esp.mr>
2026-05-06 21:54:41 +0100

    Ajout dashboard visuel Groupe 30

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, send_file
+from flask_cors import CORS
 from routes.disponibilites import bp_dispo
 from routes.reservations import bp_resa
 from routes.paiements import bp_paiement
 from scheduler import demarrer_scheduler
 
 app = Flask(__name__)
+CORS(app)
 app.config["SECRET_KEY"] = "transport2025_secret"
 
 app.register_blueprint(bp_dispo,    url_prefix="/api/disponibilites")
@@ -12,6 +14,10 @@ app.register_blueprint(bp_resa,     url_prefix="/api/reservations")
 app.register_blueprint(bp_paiement, url_prefix="/api/paiements")
 
 demarrer_scheduler()
+
+@app.route("/")
+def dashboard():
+    return send_file("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
